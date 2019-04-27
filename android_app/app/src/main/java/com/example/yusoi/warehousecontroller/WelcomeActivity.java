d6435c1d5f7f03a78fe84ddbdbb370d25771bb6f
Yusoi <bernardosilva.business@gmail.com>
2019-04-27 14:46:31 +0100

    Added android app start

---
+++ android_app/app/src/main/java/com/example/yusoi/warehousecontroller/WelcomeActivity.java
@@ -0,0 +1,48 @@
+package com.example.yusoi.warehousecontroller;
+
+import android.content.Intent;
+import android.support.v4.view.GestureDetectorCompat;
+import android.support.v4.view.MotionEventCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+
+import com.example.yusoi.warehousecontroller.R;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    private GestureDetectorCompat mDetector;
+
+    // Called when the activity is first created.
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.WelcomeTheme);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        mDetector = new GestureDetectorCompat(this, new MyGestureListener());
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event){
+        this.mDetector.onTouchEvent(event);
+        return super.onTouchEvent(event);
+    }
+
+    class MyGestureListener extends GestureDetector.SimpleOnGestureListener {
+        private static final String DEBUG_TAG = "Gestures";
+
+        private static final int MIN_SWIPE_Y_DISTANCE = 50;
+
+        @Override
+        public boolean onFling(MotionEvent event1, MotionEvent event2,
+                               float velocityX, float velocityY) {
+
+            if(event1.getY()-event2.getY() > MIN_SWIPE_Y_DISTANCE){
+                startActivity(new Intent(WelcomeActivity.this,LoginActivity.class));
+            }
+
+            return true;
+        }
+    }
+}
